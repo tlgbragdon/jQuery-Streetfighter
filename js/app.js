afe6a907c930b5a203d262ad09c3223a784bb180
tlgbragdon <tracy.bragdon@gmail.com>
2016-09-08 10:44:55 -0400

    add audio

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
    		$('.ryu-still').hide();
    		$('.ryu-ready').show();
    		
-   })
+   })			// omit ; for chaining...
    .mouseleave(function() {
    		$('.ryu-ready').hide();
    	    $('.ryu-still').show();
    })
    .mousedown(function() {
+   		playHadouken();
    		$('.ryu-ready').hide();
    		$('.ryu-throwing').show();
    		$('.hadouken').show().animate(
@@ -30,3 +31,9 @@ $(document).ready(function() {
 
 
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
